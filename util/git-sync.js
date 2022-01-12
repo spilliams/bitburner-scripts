306385f107ffd059caa2bf0c5453ebe2a4ee5c47
Spencer Williams <s@spencerenglish.com>
2022-01-12 12:46:28 -0800

    maybe this is the magic combo?

--- util/git-sync.js
+++ util/git-sync.js
@@ -2,31 +2,31 @@
 export async function main(ns) {
   const baseURL = "https://raw.githubusercontent.com/spilliams/bitburner-scripts/main";
   const files = [
-    "contracts/algorithmicStockTrader.js",
-    "contracts/subarrayWithmaximumSum.js",
-    "contracts/totalWaysToSum.js",
-    "notes/bootstrap.txt",
-    "util/formatMoney.js",
-    "util/git-sync.js",
-    "util/recurse.js",
-    "2.js",
-    "backdoor.js",
-    "buy-upgrade.js",
-    "contracts.js",
-    "dark.js",
-    "fix.js",
-    "hostname.js",
-    "infect.js",
-    "kill.js",
-    "list-servers.js",
-    "ls-all.js",
-    "map.js",
-    "open.js",
-    "target.js",
-    "test.js"
+    "/contracts/algorithmicStockTrader.js",
+    "/contracts/subarrayWithmaximumSum.js",
+    "/contracts/totalWaysToSum.js",
+    "/notes/bootstrap.txt",
+    "/util/formatMoney.js",
+    "/util/git-sync.js",
+    "/util/recurse.js",
+    "/2.js",
+    "/backdoor.js",
+    "/buy-upgrade.js",
+    "/contracts.js",
+    "/dark.js",
+    "/fix.js",
+    "/hostname.js",
+    "/infect.js",
+    "/kill.js",
+    "/list-servers.js",
+    "/ls-all.js",
+    "/map.js",
+    "/open.js",
+    "/target.js",
+    "/test.js"
   ];
   for (let i = 0; i < files.length; i++) {
-    const got = await ns.wget(ns.sprintf("%s/%s", baseURL, files[i]), files[i]);
+    const got = await ns.wget(ns.sprintf("%s%s", baseURL, files[i]), files[i]);
     if (got) {
       ns.tprintf("sync'd %s", files[i]);
     } else {
