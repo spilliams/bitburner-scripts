5c8dfe94bac5c77640776dc498d8b9e736bd854c
Spencer Williams <s@spencerenglish.com>
2022-01-13 21:25:54 -0800

    off by one error!

--- contracts_subarrayWithMaximumSum.js
+++ contracts_subarrayWithMaximumSum.js
@@ -7,15 +7,15 @@ export async function main(ns) {
 /** @param {NS} ns **/
 export function subarrayWithMaximumSum(ns, data) {
   let maxSum = 0;
-  for (let start = 0; start < data.length - 1; start++) {
+  for (let start = 0; start < data.length; start++) {
     for (let finish = start + 1; finish < data.length; finish++) {
       let sum = 0;
-      for (let i = start; i < finish; i++) {
+      for (let i = start; i <= finish; i++) {
         sum += data[i];
       }
       maxSum = Math.max(maxSum, sum);
       if (maxSum == sum) {
-        ns.tprintf("sum of elements from %d to %d is %d", start, finish, sum);
+        ns.tprintf("sum of elements from %d (a %d) to %d (a %d; exclusive) is %d", start, data[start], finish, data[finish], sum);
       }
     }
   }
