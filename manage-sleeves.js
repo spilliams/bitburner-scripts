ce0c755ddb5ed2de0ec568bebcf5055b3f874ce1
Spencer Williams <s@spencerenglish.com>
2022-01-16 12:08:38 -0800

    stub

---
+++ manage-sleeves.js
@@ -0,0 +1,8 @@
+// each tick (1 minute?)
+// if I have enough money to buy a new sleeve, buy it
+//
+// for each sleeve
+// if shock is higher than 10? do shock recovery
+// if sync is less than 50? do synchronize
+// else do a task. tasks are: study hacking, workout, work at job?
+//    if i have lots of money, get the sleeve to do study more or workout more (costs $)
