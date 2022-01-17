4c7667cdad786eb6cce481e68dc7f03f8f6d874e
Spencer Williams <s@spencerenglish.com>
2022-01-17 08:54:45 -0800

    fixabug

--- contracts_subarrayWithMaximumSum.js
+++ contracts_subarrayWithMaximumSum.js
@@ -1,21 +1,24 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-  let max = subarrayWithMaximumSum(ns, ns.args);
+  const data = [2, 2, -5, -6, -9, 7, -2, -2, -6];
+  let max = subarrayWithMaximumSum(ns, data);
   ns.tprintf("max sum is %d", max);
 }
 
 /** @param {NS} ns **/
 export function subarrayWithMaximumSum(ns, data) {
+  ns.tprintf("data: %v", data);
   let maxSum = 0;
+
   for (let start = 0; start < data.length; start++) {
-    for (let finish = start + 1; finish < data.length; finish++) {
+    for (let finish = start; finish < data.length; finish++) {
       let sum = 0;
       for (let i = start; i <= finish; i++) {
         sum += data[i];
       }
       maxSum = Math.max(maxSum, sum);
       if (maxSum == sum) {
-        ns.tprintf("sum of elements from %d (a %d) to %d (a %d; exclusive) is %d", start, data[start], finish, data[finish], sum);
+        ns.tprintf("sum of elements from %d (a %d) to %d (a %d) is %d", start, data[start], finish, data[finish], sum);
       }
     }
   }
