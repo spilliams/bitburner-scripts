8f2c30b695f99998cc686672148e0c3f3f3ba57f
Spencer Williams <s@spencerenglish.com>
2022-01-18 09:49:55 -0800

    this one's done too

--- contracts_uniquePathsGrid.js
+++ contracts_uniquePathsGrid.js
@@ -1,16 +1,35 @@
 /** @param {NS} ns **/
 export async function main(ns) {
   // ns.tprintf("%d paths", uniquePathsInAGrid1(ns, [5, 11]));
-
-  ns.tprintf("%d paths", uniquePathsInAGrid2(ns, [
-    [0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 1],
-    [0, 0, 0, 1, 1, 1],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0]
-  ]));
+  // const data = [
+  //   [0, 0, 0, 0, 0, 1],
+  //   [0, 0, 0, 0, 0, 0],
+  //   [0, 1, 0, 1, 0, 0],
+  //   [0, 0, 0, 0, 0, 1],
+  //   [0, 0, 0, 1, 1, 1],
+  //   [0, 0, 0, 0, 0, 0],
+  //   [0, 0, 0, 0, 1, 0]
+  // ]; ok!
+  const data = [
+    [0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 1, 0, 0]
+  ];
+  // const data = [
+  //   [0,0,0,0,0,0,1,0,0,0,1],
+  //   [0,1,0,0,0,0,0,0,0,0,0],
+  //   [0,0,0,1,1,0,0,0,1,0,0],
+  //   [0,0,0,0,0,0,0,0,0,0,0],
+  //   [0,0,0,0,0,0,1,0,0,1,0],
+  //   [1,0,0,0,0,0,1,0,0,0,0],
+  //   [0,0,0,0,0,1,0,0,0,0,0],
+  //   [0,1,0,0,0,0,0,0,0,0,0],
+  //   [0,0,0,0,0,1,1,0,0,0,0],
+  //   [0,0,0,0,0,0,0,1,0,0,0]
+  // ];
+  ns.tprintf("%d paths", uniquePathsInAGrid2(ns, data));
 }
 
 export function uniquePathsInAGrid1(ns, data) {
@@ -61,13 +80,27 @@ export function uniquePathsInAGrid2(ns, data) {
   for (let r = 0; r < rows; r++) {
     for (let c = 0; c < cols; c++) {
       if (data[r][c] === 0) data[r][c] = "";
-      if (data[r][c] === 1) data[r][c] = 0;
-      if (r == 0 || c == 0) data[r][c] = 1;
+      if (data[r][c] === 1) {
+        data[r][c] = 0;
+        continue;
+      }
     }
   }
-  data[0][0] = 0;
+  data[0][0] = 1;
 
   ns.tprintf("after cleaning:\n%s", sprintGrid(data));
+
+  // first row, first col
+  for (let c = 1; c < cols; c++) {
+    if (data[0][c] === 0) continue;
+    data[0][c] = data[0][c - 1];
+  }
+  for (let r = 1; r < rows; r++) {
+    if (data[r][0] === 0) continue;
+    data[r][0] = data[r - 1][0];
+  }
+
+  ns.tprintf("after initializing:\n%s", sprintGrid(data));
 
   // now count em!
   for (let r = 1; r < rows; r++) {
