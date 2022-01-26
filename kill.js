f0aa478802aa5dffe105f372004862b7b4fef11e
Spencer Williams <s@spencerenglish.com>
2022-01-25 22:00:06 -0800

    runnint with no args should kill all

--- kill.js
+++ kill.js
@@ -2,9 +2,9 @@ import { scanAll } from "util_recurse.js";
 
 /** @param {NS} ns **/
 export async function main(ns) {
-  if (ns.args.length != 1) {
-    ns.tprint("requires 1 arg: script to kill");
-    return;
+  let scriptToKill = "all";
+  if (ns.args.length == 1) {
+    scriptToKill = ns.args[0];
   }
 
   const scanning = scanAll(ns);
@@ -12,7 +12,12 @@ export async function main(ns) {
 
   for (let i = 0; i < scanning.length; i++) {
     const host = scanning[i];
-    if (ns.scriptKill(ns.args[0], host)) {
+    if (host === "home") {
+      continue;
+    }
+    if (scriptToKill === "all" && ns.killall(host)) {
+      ns.tprintf("killed all on %s", host);
+    } else if (ns.scriptKill(ns.args[0], host)) {
       ns.tprintf("killed %s on %s", ns.args[0], host);
     }
   }
