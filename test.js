a825c7a2347c4d0a7e883313a67f35d07ce47b4c
Spencer Williams <s@spencerenglish.com>
2022-01-25 22:00:43 -0800

    playing around

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-  let newArgs = ns.args.slice(1);
-  ns.tprint(newArgs.length);
+  const rs = ns.getRunningScript(ns.args[0], ns.args[1], ...ns.args.slice(2));
+  // ns.tprintf("%v (%s)", rs, typeof rs);
+  if (rs == null) {
+    ns.tprintf("null response");
+    return;
+  }
+  const keys = Object.keys(rs);
+  for (let i = 0; i < keys.length; i++) {
+    ns.tprintf("%s: %v", keys[i], rs[keys[i]]);
+  }
 }
