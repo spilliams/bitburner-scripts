9313677827d2c40bfabf2ac06ad02027abf79ae3
Spencer Williams <s@spencerenglish.com>
2022-01-25 22:00:19 -0800

    don't bug if it's at max ram

--- list-servers.js
+++ list-servers.js
@@ -1,12 +1,20 @@
 import { formatMoney } from "util_formatMoney.js";
+import { formatMem } from "util_formatMem.js";
 
 // lists purchased servers, and their maximum RAM
 
 /** @param {NS} ns **/
 export async function main(ns) {
+  const fullRAM = ns.getPurchasedServerMaxRam();
   let servers = ns.getPurchasedServers();
   for (let i = 0; i < servers.length; i++) {
     let ram = ns.getServerMaxRam(servers[i])
-    ns.tprintf("%s: %dGB (%s to upgrade)", servers[i], ram, formatMoney(ns.getPurchasedServerCost(ram * 2)));
+
+    let upgradeS = "(fully upgraded)";
+    if (ram * 2 < fullRAM) {
+      upgradeS = ns.sprintf("(%s to upgrade)", formatMoney(ns.getPurchasedServerCost(ram * 2)));
+    }
+
+    ns.tprintf("%s: %s %s", servers[i], formatMem(ram), upgradeS);
   }
 }
