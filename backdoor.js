a39a628e82630200721025d9bff2409ac0eabc86
Spencer Williams <s@spencerenglish.com>
2022-01-31 19:41:19 -0800

    i don't need to backdoor *all* the servers

--- backdoor.js
+++ backdoor.js
@@ -1,18 +1,34 @@
-import { scanAll } from "util_recurse.js";
+// import { scanAll } from "util_recurse.js";
+import { breakIt } from "infect.js";
 
 /** @param {NS} ns **/
 export async function main(ns) {
-  const servers = scanAll(ns);
-  const promises = [];
+  // const servers = scanAll(ns);
+  const servers = ["CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z"]
+  let count = 0;
 
   for (let i = 0; i < servers.length; i++) {
     const host = servers[i];
-    if (!ns.hasRootAccess(host)) {
-      continue;
-    }
-
-    promises.push(ns.installBackdoor(host));
+    let done = await backdoor(ns, host);
+    if (done) count++;
   }
 
-  await Promise.all(promises);
+  ns.tprintf("backdoored %d servers", count);
+}
+
+/** @param {NS} ns **/
+async function backdoor(ns, host) {
+  if (!ns.hasRootAccess(host)) {
+    let broken = await breakIt(ns, host);
+    if (!broken) return false;
+  }
+
+  const hackReqd = ns.getServerRequiredHackingLevel(host);
+  if (ns.getHackingLevel() < hackReqd) {
+    ns.tprintf("can't backdoor %s until hou have %d hacking", host, hackReqd);
+    return false;
+  }
+  await ns.installBackdoor(host);
+  ns.tprintf("backdoored %s", host);
+  return true;
 }
